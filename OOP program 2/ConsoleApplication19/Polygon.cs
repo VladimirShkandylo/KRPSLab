0d74d18362a2d4a79243b1d7f18a74733fab3d63
Vladimir <vladimir.shkandylo@gmail.com>
2018-03-06 19:03:41 +1300

    Add files via upload

---
+++ OOP program 2/ConsoleApplication19/Polygon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication19
+{
+    class Polygon
+    {
+        public int NumberOfSides { get; set; }
+        public string name;
+
+        public Polygon()
+        {
+            NumberOfSides = 0;
+        }
+        public Polygon(int numberOfSides)
+        {
+            NumberOfSides = numberOfSides;
+            if (numberOfSides == 3)
+            {
+                name = "triangle";
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
